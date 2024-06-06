728a40606a7704786e538fc36595514d28aabf0a
shoroqsalameh <94695708+shoroqsalameh@users.noreply.github.com>
2024-06-06 07:43:04 +0300

    Update form

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useRef, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import '../compnant/css/Login.css';
+import SVG from '../assest/todo-svg.svg'
 
 const Login = () => {
   const emailRef = useRef();
@@ -30,34 +31,36 @@ const Login = () => {
   return (
     <Container fluid className="login-page">
       <Row className="justify-content-center align-items-center min-vh-100">
-        <Col xs={12} md={6} lg={4}>
-          <div className="text-center mb-4">
-            <h1>Tan<span className="highlight">Z</span>em</h1>
+        <Col >
+          <div className="brand ">
+            <h1>Ta<span className="highlight">NZ</span>em</h1>
           </div>
           <Card className="login-card">
             <Card.Body>
-              <h2 className="text-center mb-4">Sign In</h2>
+              <h2 className="login-container ">Sign In</h2>
               {error && <Alert variant="danger">{error}</Alert>}
               <Form onSubmit={handleSubmit}>
                 <Form.Group id="email">
-                  <Form.Label>Email</Form.Label>
                   <Form.Control type="email" placeholder="Enter your Email" ref={emailRef} required />
                 </Form.Group>
                 <Form.Group id="password">
-                  <Form.Label>Password</Form.Label>
                   <Form.Control type="password" placeholder="Enter password" ref={passwordRef} required />
                 </Form.Group>
-                <div className="text-right mb-3">
+                <div className="text-right ">
                   <Link to="/forgot-password" className="text-decoration-none d-block mb-3">Forgot password?</Link>
                 </div>
                 <Button disabled={loading} className="w-100 mb-3" type="submit">Login</Button>
               </Form>
+                 <div className="text-center ">
+                  <span className="d-block ">Don't have an account?</span>
+                  <Link to="/signup" className="text-decoration-none">Sign Up</Link>
+                  </div>
             </Card.Body>
           </Card>
-          <div className="text-center mt-2">
-            <span className="d-block mb-2">Don't have an account?</span>
-            <Link to="/signup" className="text-decoration-none">Sign Up</Link>
-          </div>
+         
+        </Col>
+        <Col  className="illustration text-center d-none d-md-block">
+          <img src={SVG} alt="Illustration" />
         </Col>
       </Row>
     </Container>
@@ -65,3 +68,4 @@ const Login = () => {
 };
 
 export default Login;
+ 
