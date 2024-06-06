0c6f632536b56de89292863e52e5428b2b23b50a
shoroqsalameh <94695708+shoroqsalameh@users.noreply.github.com>
2024-06-06 07:58:56 +0300

    delete Class

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 const App = () => {
   return (
     <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
+      
     >
       <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
